44ede26d5d18b95694e1ebba9494261dbdae85a1
kangkujun <donato14@naver.com>
2022-04-25 17:21:06 +0900

    Add Date Data

--- assets/data/_contentData.js
+++ assets/data/_contentData.js
@@ -2,32 +2,38 @@ let contents = [
     {
         id : 0,
         title : "자바스크립트 연습하기",
-        desc : "자바스크립트로 블로그 내의 게시글을 구현할 것 이다. Array에 Object를 활용하여 데이터.js 파일에서 손쉽게 데이터를 뽑아낼 수 있도록 할 것이다. 게시글 당 3-5줄의 설명이 들어가면 딱 좋을 것 같다. 그래야 메인페이지에서 간단한 내용을 보여주고 글 번호 클릭시 자세한 내용을 확인 할 수 있는 글 페이지로 이동하여 사람들에게 글을 읽을 수 있도록 한다"
+        desc : "자바스크립트로 블로그 내의 게시글을 구현할 것 이다. Array에 Object를 활용하여 데이터.js 파일에서 손쉽게 데이터를 뽑아낼 수 있도록 할 것이다. 게시글 당 3-5줄의 설명이 들어가면 딱 좋을 것 같다. 그래야 메인페이지에서 간단한 내용을 보여주고 글 번호 클릭시 자세한 내용을 확인 할 수 있는 글 페이지로 이동하여 사람들에게 글을 읽을 수 있도록 한다",
+        date : "2022/4/25"
     },
     {
         id : 1,
         title : "스프링을 거의 잊어버린게 너무 창피하다",
-        desc : "거의 6개월을 센터에서 JSP, Spring을 배운 것은데 며칠전부터 확인해보니 거의 까먹은게 80%인 것 같다. 머리로는 어떻게 했던게 슬슬 기억이 나는 것 같은데;;; 막상 기본 셋팅하고 하려면 숨이 턱턱 막힌다. 아무래도 공부를 좀 더 열심히 해야 할 것 같은데 그게 마냥 쉽지는 않다.. 어디서부터 어떻게 공부를 해야할 지 감이 좀 안잡히는 경향이 있다. 내일은 책을 가지고 오고, 집에서도 좀 부족한 부분 채워나가는 노력을 해야 할 것 같다."
+        desc : "거의 6개월을 센터에서 JSP, Spring을 배운 것은데 며칠전부터 확인해보니 거의 까먹은게 80%인 것 같다. 머리로는 어떻게 했던게 슬슬 기억이 나는 것 같은데;;; 막상 기본 셋팅하고 하려면 숨이 턱턱 막힌다. 아무래도 공부를 좀 더 열심히 해야 할 것 같은데 그게 마냥 쉽지는 않다.. 어디서부터 어떻게 공부를 해야할 지 감이 좀 안잡히는 경향이 있다. 내일은 책을 가지고 오고, 집에서도 좀 부족한 부분 채워나가는 노력을 해야 할 것 같다.",
+        date : "2022/4/25"
     },
     {
         id : 2,
         title : "이제부턴 lorem으로 대체한다1",
-        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nesciunt consequuntur ducimus nam enim reprehenderit error? Aliquid incidunt provident, et sed explicabo amet asperiores suscipit praesentium inventore quasi modi quibusdam quisquam, atque quia error cum facere. Earum commodi, aliquid, totam explicabo reiciendis ut culpa dignissimos dolorum distinctio, consectetur numquam! Illo distinctio"
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nesciunt consequuntur ducimus nam enim reprehenderit error? Aliquid incidunt provident, et sed explicabo amet asperiores suscipit praesentium inventore quasi modi quibusdam quisquam, atque quia error cum facere. Earum commodi, aliquid, totam explicabo reiciendis ut culpa dignissimos dolorum distinctio, consectetur numquam! Illo distinctio",
+        date : "2022/4/25"
     },
     {
         id : 3,
         title : "이제부턴 lorem으로 대체한다2",
-        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nesciunt consequuntur ducimus nam enim reprehenderit error? Aliquid incidunt provident, et sed explicabo amet asperiores suscipit praesentium inventore quasi modi quibusdam quisquam, atque quia error cum facere. Earum commodi, aliquid, totam explicabo reiciendis ut culpa dignissimos dolorum distinctio, consectetur numquam! Illo distinctio"
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nesciunt consequuntur ducimus nam enim reprehenderit error? Aliquid incidunt provident, et sed explicabo amet asperiores suscipit praesentium inventore quasi modi quibusdam quisquam, atque quia error cum facere. Earum commodi, aliquid, totam explicabo reiciendis ut culpa dignissimos dolorum distinctio, consectetur numquam! Illo distinctio",
+        date : "2022/4/25"
     },
     {
         id : 4,
         title : "이제부턴 lorem으로 대체한다3",
-        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nesciunt consequuntur ducimus nam enim reprehenderit error? Aliquid incidunt provident, et sed explicabo amet asperiores suscipit praesentium inventore quasi modi quibusdam quisquam, atque quia error cum facere. Earum commodi, aliquid, totam explicabo reiciendis ut culpa dignissimos dolorum distinctio, consectetur numquam! Illo distinctio"
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nesciunt consequuntur ducimus nam enim reprehenderit error? Aliquid incidunt provident, et sed explicabo amet asperiores suscipit praesentium inventore quasi modi quibusdam quisquam, atque quia error cum facere. Earum commodi, aliquid, totam explicabo reiciendis ut culpa dignissimos dolorum distinctio, consectetur numquam! Illo distinctio",
+        date : "2022/4/25"
     },
     {
         id : 5,
         title : "이제부턴 lorem으로 대체한다4",
-        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nesciunt consequuntur ducimus nam enim reprehenderit error? Aliquid incidunt provident, et sed explicabo amet asperiores suscipit praesentium inventore quasi modi quibusdam quisquam, atque quia error cum facere. Earum commodi, aliquid, totam explicabo reiciendis ut culpa dignissimos dolorum distinctio, consectetur numquam! Illo distinctio"
+        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga nesciunt consequuntur ducimus nam enim reprehenderit error? Aliquid incidunt provident, et sed explicabo amet asperiores suscipit praesentium inventore quasi modi quibusdam quisquam, atque quia error cum facere. Earum commodi, aliquid, totam explicabo reiciendis ut culpa dignissimos dolorum distinctio, consectetur numquam! Illo distinctio",
+        date : "2022/4/25"
     },
     
 ]
